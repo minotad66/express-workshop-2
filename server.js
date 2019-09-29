74d219cf8d89dc967a7362af48b83d0eb0a6813b
Darwin <50497333+minotad66@users.noreply.github.com>
2019-09-29 02:38:40 -0500

    actualizacion

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const fs = require("fs");
-const readPosts = require("./helpers/readPosts.js");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -31,22 +30,27 @@ app.get("/posts", function(req, res) {
   res.sendFile(__dirname + "/data/posts.json");
 });
 
-app.get("/posts/:id", function(req, res) {
-  const postId = req.params.id;
-  const filePath = __dirname + "/data/posts.json";
-  const callbackFunction = function(error, file) {
+app.get('/posts/:id', function (req, res) {
+  const filePath = __dirname + '/data/posts.json';
+  const callbackFunction = function (error, file) {
     const fileData = file.toString();
     const postsJson = JSON.parse(fileData);
-    const first = [postsJson[0]];
-    res.render("posts", {
-      title: first,
-      summary: first,
-      content: first
-    });
+    let post = postsJson.filter(id => id.postId === req.params.id);
+    console.log(post);
+    //const first = post;
+    if(post != []){
+      res.render(`posts/post`,
+      {
+        title: post[0].title,
+        summary: post[0].summary,
+        content: post[0].content,
+      });
+    }
+    else{
+      res.render(`post/post`,{title:'no exite esa publicacion'});
+    }
   };
-
   fs.readFile(filePath, callbackFunction);
-  res.send(req.params);
 });
 
 app.get("/my-cv", function(req, res) {
@@ -63,6 +67,47 @@ app.get("/admin", function(req, res) {
   });
 });
 
+app.get("/admin", function(req, res) {
+  res.render("admin", {
+    title: "admin",
+    subholder: "Our Admin"
+  });
+});
+
+app.get("/admin", function(req, res) {
+  res.render("admin", {
+    title: "admin",
+    subholder: "Our Admin"
+  });
+});
+
+app.post("/admin", function(req, res) {
+  const fs = require("fs");
+  const filePath = __dirname + "/../data/posts.json";
+  
+  const savePost = (newPost, callback) => {
+    fs.readFile(filePath, (error, file) => {
+      if (error) {
+        console.error("error reading the file");
+        console.error(error);
+        return callback(error);
+      }
+  
+      const posts = JSON.parse(file.toString());
+      posts.splice(0, 0, newPost);
+  
+      fs.writeFile(filePath, JSON.stringify(posts, null, 2), error => {
+        if (error) {
+          console.error("error writing to the file");
+          console.error(error);
+          return callback(error);
+        }
+        callback();
+      });
+    });
+  };
+});
+
 app.get("/contact", function(req, res) {
   res.render("contact", {
     title: "contact",
@@ -74,3 +119,4 @@ app.get("/contact", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
+
